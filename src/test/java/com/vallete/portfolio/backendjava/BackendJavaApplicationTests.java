daa8d69048a6bfb6ed2721fbd01080da0385a0fb
Juener <juener@gmail.com>
2022-12-05 22:56:18 +0100

    spring boot default project

---
+++ src/test/java/com/vallete/portfolio/backendjava/BackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vallete.portfolio.backendjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
